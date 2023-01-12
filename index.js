077ed548ab44dd7f4fc26a4dacc25871067bb06a
Alexander <102146514+Alexxxlev@users.noreply.github.com>
2023-01-12 15:46:24 +0300

    простая и понятная логика

---
+++ index.js
@@ -0,0 +1,26 @@
+const valueInputNode = document.querySelector('.js-value-input');
+const currencyCelectorNode = document.querySelector('.js-currency-selector');
+const outputNode = document.querySelector('.js-output');
+
+function getInput() {
+  return {
+    rub: Number(valueInputNode.value),
+    currency: currencyCelectorNode.value
+  }
+}
+
+function render(result) {
+  outputNode.innerText = result;
+}
+
+valueInputNode.addEventListener('input' ,function() {
+  const result = convert(getInput());
+
+  render(result);
+});
+
+currencyCelectorNode.addEventListener('change', function() {
+  const result = convert(getInput());
+
+  render(result);
+});
